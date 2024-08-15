aeaff3d169da0d8af1bfcca136ec254c20b3e7bc
AbuBasssam <166512044+AbuBasssam@users.noreply.github.com>
2024-08-15 21:49:17 +0300

    Add files via upload

---
+++ DVLD_WinForm_PresentationLayer/Login/frmLogin.cs
@@ -0,0 +1,98 @@
+﻿using DVlD.BusinessLayer;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVlD_BusinessLayer;
+
+namespace DVLD.Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            clsUser user = clsUser.Find(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+
+            if (user != null)
+            {
+
+                if (checkBox1.Checked)
+                {
+                    //store username and password
+                    clsGlobal.RememberUsernameAndPassword(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+
+                }
+                else
+                {
+                    //store empty username and password
+                    clsGlobal.RememberUsernameAndPassword("", "");
+
+                }
+
+                //incase the user is not active
+                if (!user.IsActive)
+                {
+
+                    txtUserName.Focus();
+                    MessageBox.Show("Your accound is not Active, Contact Admin.", "In Active Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                clsGlobal.CurrentUser = user;
+                this.Hide();
+                frmMain frm = new frmMain();
+                frm.ShowDialog();
+                if (!checkBox1.Checked)
+                {
+
+                    txtPassword.Text = string.Empty;
+                    txtUserName.Text = string.Empty;
+                }
+                this.Show();
+                
+
+
+            }
+            else
+            {
+                txtUserName.Focus();
+                MessageBox.Show("Invalid Username/Password.", "Wrong Credintials", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+           
+
+            string UserName = "", Password = "";
+
+            if (clsGlobal.GetStoredCredential(ref UserName, ref Password))
+            {
+                txtUserName.Text = UserName;
+                txtPassword.Text = Password;
+                checkBox1.Checked = true;
+            }
+            else
+                checkBox1.Checked = false;
+
+
+        }
+
+        
+    }
+}
