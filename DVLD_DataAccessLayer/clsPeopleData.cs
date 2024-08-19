7903a4f6ac72a3159fc1b4925f310a6326b6680c
Abdul Rahman <programmerabubassam@gmail.com>
2024-08-19 15:22:09 +0300

    Add People DTO

--- DVLD_DataAccessLayer/clsPeopleData.cs
+++ DVLD_DataAccessLayer/clsPeopleData.cs
@@ -14,8 +14,53 @@ namespace DVLD_DataAccessLayer
 {
     public class clsPeopleData
     {
-        
-        
+        public class PeopleDTO
+        {
+
+            public Nullable<int> PersonID { set; get; }
+            public string NationalNo { set; get; }
+            public string FirstName { set; get; }
+            public string SecondName { set; get; }
+            public string ThirdName { set; get; }
+            public string LastName { set; get; }
+            public DateTime DateOfBirth { set; get; }
+            public byte Gender { set; get; }
+            public string Address { set; get; }
+            public string Phone { set; get; }
+            public string Email { set; get; }
+            public byte Nationality { set; get; }
+
+            private string _ImagePath;
+            public string ImagePath
+            {
+                get { return _ImagePath; }
+                set { _ImagePath = value; }
+            }
+            private PeopleDTO(Nullable<int> PersonID, string NationalNo, string FirstName, string SecondName
+                , string ThirdName, string LastName, DateTime DateOfBirth, byte Gender,
+                string Address, string Phone, string Email, byte Nationality, string ImagePath)
+            {
+                this.PersonID = PersonID;
+                this.NationalNo = NationalNo;
+                this.FirstName = FirstName;
+                this.SecondName = SecondName;
+                this.ThirdName = ThirdName;
+                this.LastName = LastName;
+                this.DateOfBirth = DateOfBirth;
+                this.Gender = Gender;
+                this.Address = Address;
+                this.Phone = Phone;
+                this.Email = Email;
+                this.Nationality = Nationality;
+                this.ImagePath = ImagePath;
+
+            }
+
+
+        }
+
+
+
         public static bool FindByNationalNo(ref Nullable<int> PersonID, string NationalNO,
             ref string FirstName, ref string SecondName, ref string ThirdName, ref string LastName
             , ref DateTime DateOfBirth, ref byte Gender, ref string Address,
