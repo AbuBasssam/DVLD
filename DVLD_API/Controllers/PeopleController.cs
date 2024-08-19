8b5ddb9c5182a8a7b0f4f4af84154b48e25d1fb1
Abdul Rahman <programmerabubassam@gmail.com>
2024-08-19 15:07:36 +0300

    Start API Layer

---
+++ DVLD_API/Controllers/PeopleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DVLD_API.Controllers
+{
+    [Route("api/People")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+    }
+}
