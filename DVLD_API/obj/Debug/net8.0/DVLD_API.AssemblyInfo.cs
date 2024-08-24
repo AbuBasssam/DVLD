bd029c64bdf9230ffba6256c7f39736e40e92257
Abdul Rahman <programmerabubassam@gmail.com>
2024-08-24 11:10:43 +0300

    Remove the UI Layer

--- DVLD_API/obj/Debug/net8.0/DVLD_API.AssemblyInfo.cs
+++ DVLD_API/obj/Debug/net8.0/DVLD_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DVLD_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483dca05668971751c7d83f5f9e954cee5d8756d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8cc3132c7a3fe82f73278e00d346cbba0343945")]
 [assembly: System.Reflection.AssemblyProductAttribute("DVLD_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DVLD_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
