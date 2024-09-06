ab08e7445039116363c86b17f5abc8694fc22c79
Abdul Rahman <programmerabubassam@gmail.com>
2024-09-06 15:48:57 +0300

    Convert DAL Application to support async

---
+++ DVLD_DataAccessLayer/Entities/ApplicationDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccessLayer.Entities
+{
+    public class ApplicationDTO
+    {
+        public int ApplicationID { get; set; }
+        public int ApplicationPersonID {  get; set; }
+
+        public DateTime ApplicationDate { get; set; }
+
+        public int ApplicationTypeID { get; set; }
+        
+        public byte Staute { get; set; }
+        
+        public DateTime LastStauteDate { get; set; }
+        
+        public float PeadFees { get; set; }
+        
+        public  int CreatedBy {  get; set; }
+
+        public ApplicationDTO(int ApplicationID, int applicationPersonID, DateTime applicationDate, int applicationTypeID, byte staute, DateTime lastStauteDate, float peadFees, int createdBy)
+        {
+            this.ApplicationID = ApplicationID;
+            ApplicationPersonID = applicationPersonID;
+            ApplicationDate = applicationDate;
+            ApplicationTypeID = applicationTypeID;
+            Staute = staute;
+            LastStauteDate = lastStauteDate;
+            PeadFees = peadFees;
+            CreatedBy = createdBy;
+        }
+    }
+}
