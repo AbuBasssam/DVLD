1a1ed3b59bc14bc13b4c777beb9e41c6791b456f
Abdul Rahman <programmerabubassam@gmail.com>
2024-09-08 09:38:31 +0300

    Convert BLL Application to support async

---
+++ DVLD_BusinessLayer/Interfaces/IBLLApplication.cs
@@ -0,0 +1,44 @@
+﻿using DVLD_DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DVlD_BusinessLayer.clsApplication;
+
+namespace DVlD_BusinessLayer.Interfaces
+{
+    public interface IBLLApplication
+    {
+        public Nullable<int> ApplicationID { get; set; }
+
+        public int ApplicationPersonID { get; set; }
+
+        public DateTime ApplicationDate { get; set; }
+
+        public enApplicationType ApplicationTypeID { get; set; }
+
+        public clsApplication.enApplicationStatus ApplicationStatus { get; set; }
+        public DateTime LastStauteDate { get; set; }
+
+        public float PaidFees { get; set; }
+
+        public int CreatedBy { get; set; }
+        Task<clsApplication> Find(int ApplicationID);
+        Task<int?> AddNewApplication(ApplicationDTO ADTO);
+        Task<bool> UpdateApplication(ApplicationDTO ADTO);
+        Task<bool> DeleteApplication(int ApplicationID);
+        Task<bool> Cancel();
+        Task<bool> SetComplete();
+        Task<bool> Delete();
+        Task<bool> IsApplicationExist(int ApplicationID);
+        Task<bool> DoesPersonHaveActiveApplication(int PersonID, int ApplicationTypeID);
+        Task<bool> DoesPersonHaveActiveApplication(int ApplicationTypeID);
+        Task<int> GetActiveApplicationID(int PersonID, clsApplication.enApplicationType ApplicationTypeID);
+        Task<int> GetActiveApplicationIDForLicenseClass(int PersonID, clsApplication.enApplicationType ApplicationTypeID, int LicenseClassID);
+        Task<int> GetActiveApplicationID(clsApplication.enApplicationType ApplicationTypeID);
+
+
+
+    }
+}
--- DVLD_BusinessLayer/clsDetainedLicense.cs
+++ DVLD_BusinessLayer/clsDetainedLicense.cs
@@ -1,4 +1,6 @@
 ﻿using DVLD_DataAccessLayer;
+using DVLD_DataAccessLayer.Entities;
+using DVLD_DataAccessLayer.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,167 +12,82 @@ namespace DVlD_BusinessLayer
 {
     public class clsDetainedLicense
     {
-        public enum enMode { AddNew = 0, Update = 1 };
-        public enMode Mode = enMode.AddNew;
-
-
+        private IDALDetainedLicense _DAL;
         public int DetainID { set; get; }
         public int LicenseID { set; get; }
         public DateTime DetainDate { set; get; }
 
         public float FineFees { set; get; }
         public int CreatedByUserID { set; get; }
-        public clsUser CreatedByUserInfo { set; get; }
         public bool IsReleased { set; get; }
-        public DateTime ReleaseDate { set; get; }
-        public int ReleasedByUserID { set; get; }
-        public clsUser ReleasedByUserInfo { set; get; }
-        public int ReleaseApplicationID { set; get; }
+        public DateTime? ReleaseDate { set; get; }
+        public int? ReleasedByUserID { set; get; }
+        public int? ReleaseApplicationID { set; get; }
 
-        public clsDetainedLicense()
-
+        public clsDetainedLicense(IDALDetainedLicense dALDetainedLicense)
         {
-            this.DetainID = -1;
-            this.LicenseID = -1;
-            this.DetainDate = DateTime.Now;
-            this.FineFees = 0;
-            this.CreatedByUserID = -1;
-            this.IsReleased = false;
-            this.ReleaseDate = DateTime.MaxValue;
-            this.ReleasedByUserID = 0;
-            this.ReleaseApplicationID = -1;
-
-
-
-            Mode = enMode.AddNew;
+            _DAL = dALDetainedLicense;
 
         }
 
-        public clsDetainedLicense(int DetainID,
-            int LicenseID, DateTime DetainDate,
-            float FineFees, int CreatedByUserID,
-            bool IsReleased, DateTime ReleaseDate,
-            int ReleasedByUserID, int ReleaseApplicationID)
+        private clsDetainedLicense(IDALDetainedLicense dALDetainedLicense,DetainedLicenseDTO DLDTO)
 
         {
-            this.DetainID = DetainID;
-            this.LicenseID = LicenseID;
-            this.DetainDate = DetainDate;
-            this.FineFees = FineFees;
-            this.CreatedByUserID = CreatedByUserID;
-            //this.CreatedByUserInfo = clsUser.FindByUserID(this.CreatedByUserID).Result;
-            this.IsReleased = IsReleased;
-            this.ReleaseDate = ReleaseDate;
-            this.ReleasedByUserID = ReleasedByUserID;
-            this.ReleaseApplicationID = ReleaseApplicationID;
-            //this.ReleasedByUserInfo = clsUser.FindByPersonID(this.ReleasedByUserID).Result;
-            Mode = enMode.Update;
+            this.DetainID = DLDTO.DetainID;
+            this.LicenseID = DLDTO.LicenseID;
+            this.DetainDate = DLDTO.DetainDate;
+            this.FineFees =DLDTO. FineFees;
+            this.CreatedByUserID = DLDTO.CreatedByUserID;
+            this.IsReleased = DLDTO.IsReleased;
+            this.ReleaseDate =DLDTO.ReleaseDate;
+            this.ReleasedByUserID =DLDTO.ReleasedByUserID;
+            this.ReleaseApplicationID = DLDTO.ReleaseApplicationID;
         }
 
-        private bool _AddNewDetainedLicense()
+        public async Task<int?> AddNewDetainedLicense(DetainedLicenseDTO DLDTO)
         {
             //call DataAccess Layer 
 
-            this.DetainID = clsDetainedLicenseData.AddNewDetainedLicense(
-                this.LicenseID, this.DetainDate, this.FineFees, this.CreatedByUserID);
-
-            return (this.DetainID != -1);
+            return  await _DAL.AddNewDetainedLicense(DLDTO);
         }
 
-        private bool _UpdateDetainedLicense()
+        public async Task<bool> UpdateDetainedLicense(DetainedLicenseDTO DLDTO)
         {
             //call DataAccess Layer 
 
-            return clsDetainedLicenseData.UpdateDetainedLicense(
-                this.DetainID, this.LicenseID, this.DetainDate, this.FineFees, this.CreatedByUserID);
+            return await _DAL.UpdateDetainedLicense(DLDTO);
         }
 
-        public static clsDetainedLicense Find(int DetainID)
+        public async Task< clsDetainedLicense> Find(int DetainID)
         {
-            int LicenseID = -1; DateTime DetainDate = DateTime.Now;
-            float FineFees = 0; int CreatedByUserID = -1;
-            bool IsReleased = false; DateTime ReleaseDate = DateTime.MaxValue;
-            int ReleasedByUserID = -1; int ReleaseApplicationID = -1;
-
-            if (clsDetainedLicenseData.GetDetainedLicenseInfoByID(DetainID,
-            ref LicenseID, ref DetainDate,
-            ref FineFees, ref CreatedByUserID,
-            ref IsReleased, ref ReleaseDate,
-            ref ReleasedByUserID, ref ReleaseApplicationID))
-
-                return new clsDetainedLicense(DetainID,
-                     LicenseID, DetainDate,
-                     FineFees, CreatedByUserID,
-                     IsReleased, ReleaseDate,
-                     ReleasedByUserID, ReleaseApplicationID);
-            else
-                return null;
+            DetainedLicenseDTO detainedLicenseDTO=await _DAL.GetDetainedLicenseInfoByID(DetainID);
+            return (detainedLicenseDTO!=null)? new clsDetainedLicense(_DAL,detainedLicenseDTO):null;
 
         }
 
-        public static DataTable GetAllDetainedLicenses()
+        public async Task<IEnumerable<DetainedLicenseDTO> >GetAllDetainedLicenses()
         {
-            return clsDetainedLicenseData.GetAllDetainedLicenses();
+            return await _DAL.GetAllDetainedLicenses();
 
         }
 
-        public static clsDetainedLicense FindByLicenseID(int LicenseID)
+        public async Task<clsDetainedLicense> FindByLicenseID(int LicenseID)
         {
-            int DetainID = -1; DateTime DetainDate = DateTime.Now;
-            float FineFees = 0; int CreatedByUserID = -1;
-            bool IsReleased = false; DateTime ReleaseDate = DateTime.MaxValue;
-            int ReleasedByUserID = -1; int ReleaseApplicationID = -1;
-
-            if (clsDetainedLicenseData.GetDetainedLicenseInfoByLicenseID(LicenseID,
-            ref DetainID, ref DetainDate,
-            ref FineFees, ref CreatedByUserID,
-            ref IsReleased, ref ReleaseDate,
-            ref ReleasedByUserID, ref ReleaseApplicationID))
-
-                return new clsDetainedLicense(DetainID,
-                     LicenseID, DetainDate,
-                     FineFees, CreatedByUserID,
-                     IsReleased, ReleaseDate,
-                     ReleasedByUserID, ReleaseApplicationID);
-            else
-                return null;
+            DetainedLicenseDTO detainedLicenseDTO = await _DAL.GetDetainedLicenseInfoByLicenseID(LicenseID);
+            return (detainedLicenseDTO != null) ? new clsDetainedLicense(_DAL, detainedLicenseDTO) : null;
 
         }
 
-        public bool Save()
+        
+        public async Task<bool> IsLicenseDetained(int LicenseID)
         {
-            switch (Mode)
-            {
-                case enMode.AddNew:
-                    if (_AddNewDetainedLicense())
-                    {
-
-                        Mode = enMode.Update;
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-
-                case enMode.Update:
-
-                    return _UpdateDetainedLicense();
-
-            }
-
-            return false;
+            return await _DAL.IsLicenseDetained(LicenseID);
         }
 
-        public static bool IsLicenseDetained(int LicenseID)
+        public async Task<bool> ReleaseDetainedLicense(int ReleasedByUserID, int ReleaseApplicationID)
         {
-            return clsDetainedLicenseData.IsLicenseDetained(LicenseID);
-        }
-
-        public bool ReleaseDetainedLicense(int ReleasedByUserID, int ReleaseApplicationID)
-        {
-            return clsDetainedLicenseData.ReleaseDetainedLicense(this.DetainID,
-                   ReleasedByUserID, ReleaseApplicationID);
+            ReleaseLicenseDTO RLDTO = new ReleaseLicenseDTO(this.DetainID, ReleasedByUserID, ReleaseApplicationID);
+            return await _DAL.ReleaseDetainedLicense(RLDTO);
         }
 
     }
